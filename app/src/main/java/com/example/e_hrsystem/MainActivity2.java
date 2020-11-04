23508d2dfc2a52ad717b68a5a18af533155c8c3c
Abdullah <abood.gh@Live.com>
2020-11-04 17:23:29 +0200

    - The bottom navigation bar has been edited to a drawer slide right navigation with a valid logout button

--- app/src/main/java/com/example/e_hrsystem/MainActivity2.java
+++ app/src/main/java/com/example/e_hrsystem/MainActivity2.java
@@ -1,35 +1,59 @@
 package com.example.e_hrsystem;
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.example.e_hrsystem.utils.SharedPreferencesHelper;
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-//implement the interface OnNavigationItemSelectedListener in your activity class
-public class MainActivity2 extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity2 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        //loading the default fragment
-        loadFragment(new ProfileFragment());
 
-        //getting bottom navigation view and attaching the listener
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(this);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_fragment,new ProfileFragment());
+        fragmentTransaction.commit();
     }
-
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
         Fragment fragment = null;
 
         switch (item.getItemId()) {
@@ -52,6 +76,18 @@ public class MainActivity2 extends AppCompatActivity implements BottomNavigation
             case R.id.navigation_attendance:
                 fragment = new AttendanceFragment();
                 break;
+
+            case R.id.navigation_logout:
+                findViewById(R.id.navigation_logout).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // clear the saved name that is saved in the SP, and redirect the user to the splash screen
+                        SharedPreferencesHelper.logout(MainActivity2.this);
+                        startActivity(new Intent(MainActivity2.this, SplashActivity.class));
+                        finish();
+                    }
+                });
+            break;
         }
 
         return loadFragment(fragment);
@@ -62,7 +98,7 @@ public class MainActivity2 extends AppCompatActivity implements BottomNavigation
         if (fragment != null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragment_container, fragment)
+                    .replace(R.id.container_fragment, fragment)
                     .commit();
             return true;
         }
